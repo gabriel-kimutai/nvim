9c715425de8e69621739e0760bf47d7c19c49053
gabriel-kimutai <gabrielkimutai997@gmail.com>
2024-12-28 22:30:40 +0300

    Added vim-dadbod, oil, tmux, zenbones, fugitive

--- init.lua
+++ init.lua
@@ -6,6 +6,7 @@ vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
