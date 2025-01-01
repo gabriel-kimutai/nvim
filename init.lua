263b6417c672521aefe1c69fa83647fa7edac6f0
gabriel-kimutai <gabrielkimutai997@gmail.com>
2025-01-01 16:45:20 +0300

    Mod: set softtabstop, shiftwidth and expandtab

--- init.lua
+++ init.lua
@@ -653,6 +653,7 @@ require('lazy').setup({
       --  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
       --  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
       local capabilities = require('blink.cmp').get_lsp_capabilities()
+      capabilities.textDocument.completion.completionItem.snippetSupport = true
 
       -- Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
@@ -672,11 +673,6 @@ require('lazy').setup({
             },
             staticcheck = true,
             gofumpt = true,
-            inlay_hints = true,
-          },
-          extensions = { 'tmpl', 'gohtml', 'tpl' },
-          analyses = {
-            template = true,
           },
         },
         emmet_ls = {
